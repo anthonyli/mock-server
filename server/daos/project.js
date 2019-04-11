b9060da24a1851c2c25d6c49682763343228c27b
Anthony Li <lisang881021@sina.com>
2019-04-11 21:54:13 +0800

    update:获取项目列表

--- server/daos/project.js
+++ server/daos/project.js
@@ -3,7 +3,32 @@ const STATUS_DISABLED = 0
 const ROLE_OWNER = 1
 const ROLE_MEMBER = 2
 module.exports = class {
-  async getProjectList(opts = {}) {}
+  async getProjectList(opts = {}) {
+    const { Permission, Project, sequelize } = global.M
+    await Permission.findAll({
+      attributes: {
+        include: [
+          [sequelize.col('Project.project_name'), 'name'],
+          [sequelize.col('Project.description'), 'desc']
+          // [sequelize.fn('count', sequelize.col('Project.ApiDoc.id')), 'apiNums']
+        ]
+      },
+      include: [
+        {
+          attributes: [],
+          model: Project,
+          as: 'Project',
+          where: {
+            status: STATUS_ENABLED
+          }
+        }
+      ],
+      group: ['Project.id'],
+      where: {
+        uid: opts.id
+      }
+    })
+  }
   saveProject(opts = {}, ctx) {
     const { Project, Permission, sequelize } = global.M
     let pro = {}
